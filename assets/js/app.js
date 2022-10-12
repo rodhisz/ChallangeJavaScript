25509421390066af6c2af0a03568e54877cbf23a
rodhisz <rodhisz.45@gmail.com>
2022-10-12 21:16:49 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+import { resetResult, getInputValue, setTextContent } from './functions.js'
+import changeWord from './soal1.js'
+import checkTypeNumber from './soal2.js'
+
+
+// SOAL 1
+document.querySelector('.btnChangeWord').addEventListener('click', () => {
+    const result = changeWord(
+      getInputValue('.selectedText'),
+      getInputValue('.changedText'),
+      getInputValue('.text')
+    )
+  
+    setTextContent('.resultText', result)
+  })
+
+// SOAL 2
+document.querySelector('.btnCheckTypeNumber').addEventListener('click', () => {
+  const result = checkTypeNumber(getInputValue('.givenValue'))
+  setTextContent('.resultTypeNumber', result)
+})
+
+resetResult('.resetChangeWord', '.resetText')
+resetResult('.resetCheckTypeNumber', '.resetTypeNumber')
